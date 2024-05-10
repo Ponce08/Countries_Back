efdd6fe1e0e16bc8050b320b9be1ad0888635e0a
ponce08 <yonathanponce2019@gmail.com>
2024-05-10 11:56:32 -0500

    vercel update

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const { conn } = require('./src/db.js');
 const addCountriesBDD = require('./src/controllers/addCountriesBDD.js');
 const PORT = 3001;
 
-conn
-  .sync({ force: true })
-  .then(() => {
+// conn
+//   .sync({ force: true })
+//   .then(() => {
+//     addCountriesBDD();
+//     server.listen(PORT, () => {
+//       console.log(`Server listening on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => console.error(error));
+
+const startServer = async () => {
+  try {
+    await conn.sync({ force: false }); //! true borra
     addCountriesBDD();
-    server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
-    });
-  })
-  .catch((error) => console.error(error));
+    server.listen(PORT, () => console.log(`Server raised in port: ${PORT}`));
+  } catch (error) {
+    console.log("Server not started", error.message);
+  }
+};
+
+startServer();
