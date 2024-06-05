21c37e5a5375cdd83e699140fd67d09089199c39
ponce08 <yonathanponce2019@gmail.com>
2024-06-05 17:53:42 -0500

    delete cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
-const cors = require("cors");
 
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors());
 server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
